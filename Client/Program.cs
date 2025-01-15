6b567ff8a9bf701d15992a3e92951cd28a9bbf8e
Volodymyr_Voitovych <Volodymyr_Voitovych@epam.com>
2025-01-15 18:43:42 +0200

    minor changes

---
+++ Client/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Http;
+using System.Net;
+using System.Threading.Tasks;
+using System.Text;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using HttpClient client = new HttpClient(new HttpClientHandler { UseCookies = false });
+        string url = "http://localhost:8888/";
+
+        // Send a GET request to the root URL
+        HttpResponseMessage response = await client.GetAsync(url);
+        string responseBody = await response.Content.ReadAsStringAsync();
+        Console.WriteLine($"GET Response: {responseBody}");
+
+        // Send a GET request to the MyName URL
+        response = await client.GetAsync(url + "MyName/");
+        responseBody = await response.Content.ReadAsStringAsync();
+        Console.WriteLine($"GET MyName Response: {responseBody}");
+
+        // Send requests to URLs and output response codes
+        await SendRequestAndPrintResponseCode(client, url + "Information/");
+        await SendRequestAndPrintResponseCode(client, url + "Success/");
+        await SendRequestAndPrintResponseCode(client, url + "Redirection/");
+        await SendRequestAndPrintResponseCode(client, url + "ClientError/");
+        await SendRequestAndPrintResponseCode(client, url + "ServerError/");
+
+        // Send a GET request to the MyNameByHeader URL
+        response = await client.GetAsync(url + "MyNameByHeader/");
+        if (response.Headers.TryGetValues("X-MyName", out var values))
+        {
+            string myName = string.Join(",", values);
+            Console.WriteLine($"GET MyNameByHeader Response Header X-MyName: {myName}");
+        }
+
+        // Send a GET request to the MyNameByCookies URL
+        response = await client.GetAsync(url + "MyNameByCookies/");
+        if (response.Headers.TryGetValues("Set-Cookie", out var cookieValues))
+        {
+            foreach (var cookieValue in cookieValues)
+            {
+                Console.WriteLine($"GET MyNameByCookies Response Cookie: {cookieValue}");
+            }
+        }
+
+        // Send a POST request to exit the listener
+        HttpContent content = new StringContent("", Encoding.UTF8, "application/json");
+        response = await client.PostAsync(url + "exit", content);
+        Console.WriteLine($"POST Response: {response.StatusCode}");
+    }
+
+    static async Task SendRequestAndPrintResponseCode(HttpClient client, string url)
+    {
+        HttpResponseMessage response = await client.GetAsync(url);
+        Console.WriteLine($"GET {url} Response Status Code: {response.StatusCode}");
+    }
+}
---
+++ Listener/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        HttpListener listener = new HttpListener();
+        listener.Prefixes.Add("http://localhost:8888/");
+        listener.Start();
+        Console.WriteLine("Listening for connections on http://localhost:8888/");
+
+        while (true)
+        {
+            HttpListenerContext context = await listener.GetContextAsync();
+            HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+
+            string resourcePath = GetResourcePath(request.Url.AbsolutePath);
+            Console.WriteLine($"Request received for /{resourcePath}");
+
+            if (request.HttpMethod == "POST" && resourcePath == "exit")
+            {
+                Console.WriteLine("Exit command received. Shutting down listener.");
+                response.StatusCode = (int)HttpStatusCode.OK;
+                response.Close();
+                break;
+            }
+
+            switch (resourcePath)
+            {
+                case "MyName":
+                    await HandleMyNameRequest(response);
+                    break;
+                case "MyNameByHeader":
+                    HandleMyNameByHeaderRequest(response);
+                    break;
+                case "MyNameByCookies":
+                    HandleMyNameByCookiesRequest(response);
+                    break;
+                case "Information":
+                    await HandleInformationRequest(response);
+                    break;
+                case "Success":
+                    await HandleSuccessRequest(response);
+                    break;
+                case "Redirection":
+                    await HandleRedirectionRequest(response);
+                    break;
+                case "ClientError":
+                    await HandleClientErrorRequest(response);
+                    break;
+                case "ServerError":
+                    await HandleServerErrorRequest(response);
+                    break;
+                default:
+                    await HandleDefaultRequest(response);
+                    break;
+            }
+        }
+
+        listener.Stop();
+    }
+
+    static string GetResourcePath(string absolutePath)
+    {
+        return absolutePath.Trim('/').Split('/')[0];
+    }
+
+    static async Task HandleMyNameRequest(HttpListenerResponse response)
+    {
+        string responseString = "Volodymyr Voitovych";
+        byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+        response.ContentLength64 = buffer.Length;
+        await response.OutputStream.WriteAsync(buffer, 0, buffer.Length);
+        response.Close();
+    }
+
+    static void HandleMyNameByHeaderRequest(HttpListenerResponse response)
+    {
+        response.AddHeader("X-MyName", "Volodymyr Voitovych");
+        response.StatusCode = (int)HttpStatusCode.OK;
+        response.Close();
+    }
+
+    static void HandleMyNameByCookiesRequest(HttpListenerResponse response)
+    {
+        Cookie cookie = new Cookie("MyName", "Volodymyr Voitovych");
+        response.Cookies.Add(cookie);
+        response.StatusCode = (int)HttpStatusCode.OK;
+        response.Close();
+    }
+
+    static async Task HandleInformationRequest(HttpListenerResponse response)
+    {
+        response.StatusCode = (int)HttpStatusCode.Accepted;
+        response.Close();
+    }
+
+    static async Task HandleSuccessRequest(HttpListenerResponse response)
+    {
+        response.StatusCode = (int)HttpStatusCode.OK;
+        response.Close();
+    }
+
+    static async Task HandleRedirectionRequest(HttpListenerResponse response)
+    {
+        response.StatusCode = (int)HttpStatusCode.MultipleChoices;
+        response.Close();
+    }
+
+    static async Task HandleClientErrorRequest(HttpListenerResponse response)
+    {
+        response.StatusCode = (int)HttpStatusCode.BadRequest;
+        response.Close();
+    }
+
+    static async Task HandleServerErrorRequest(HttpListenerResponse response)
+    {
+        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        response.Close();
+    }
+
+    static async Task HandleDefaultRequest(HttpListenerResponse response)
+    {
+        string responseString = "Hello World!";
+        byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+        response.ContentLength64 = buffer.Length;
+        await response.OutputStream.WriteAsync(buffer, 0, buffer.Length);
+        response.Close();
+    }
+}
